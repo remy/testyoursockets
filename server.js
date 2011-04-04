35a125875e4f4f50f06d6ce067092d829503ba98
remy <remy@remysharp.com>
2011-04-04 14:17:16 +0100

    testing change with dual remotes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var http = require('http');
- 
+
 var server = http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" })
-      res.end("Hello world\n");
+  res.writeHead(200, { "Content-Type": "text/plain" })
+  res.end("Test your sockets!\n");
 });
- 
+
 server.listen(process.env.PORT || 8001);
 
