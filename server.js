b856efaed9184ec733c693ad7a795a242cd40620
remy <remy@remysharp.com>
2011-04-08 15:22:22 +0100

    changed text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Test your sockets\n");
+  res.end("Test your sockets - beyatch\n");
 });
 
 server.listen(process.env.PORT || 8001);
