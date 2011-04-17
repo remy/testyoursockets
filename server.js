88788b036cbb43c5a24ce77d8c7035c539c71e1f
remy <remy@remysharp.com>
2011-04-17 19:26:52 +0100

    early working version of the socket testing site

--- server.js
+++ server.js
@@ -1,9 +1,60 @@
-var http = require('http');
+var connect = require('connect'),
+    parse = require('url').parse,
+    querystring = require('querystring').parse,
+    ws = require(__dirname + '/websocket-server'),
+    connections = [],
+    connected = 0;
 
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Test your sockets - beyatch\n");
+function router(app) {
+  app.get('/test', function (req, res, next) {
+    var url = parse(req.url);
+
+    socket.broadcast(url.query);
+    
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Your message has been posted\n');
+  });
+
+  app.post('/test', function (req, res, next) {
+    socket.broadcast(req.rawBody);
+    
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Your message has been posted\n');
+  });
+}
+
+var server = connect.createServer(
+  connect.bodyParser(),
+  //connect.logger(),
+  connect.static(__dirname + '/public'),
+  connect.router(router)
+);
+
+var socket = ws.createServer({
+  server: server
 });
 
-server.listen(process.env.PORT || 8001);
+socket.addListener('listening', function(){
+  console.log('Listening for connections on ' + process.ARGV[2]);
+});
 
+// Handle WebSocket Requests
+socket.addListener('connection', function(conn){
+  console.log('CONNECTION!');
+  socket.send(conn.id, connected+'');
+  socket.broadcast(connected+'');
+  
+  conn.addListener('message', function(message){
+    console.log('MESSAGE IN!');
+    socket.broadcast(message);
+  });
+});
+
+socket.addListener('close', function(conn){
+  connected--;
+  console.log('CLOSED!');
+  conn.broadcast(connected+'');
+});
+
+console.log('Listening on ' + (process.ARGV[2] || 80));
+server.listen(parseInt(process.ARGV[2]) || 80);
